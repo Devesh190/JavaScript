3bd4915506ba2875842b052da653ec968779b739
Devesh190 <deveshchoudhary373@gmail.com>
2024-05-08 05:03:34 +0000

    Datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,4 +2,14 @@ let name = "Devesh"
 let number = 847843
 let isLoggedIn = false
 
-//number => 2 to 
+// number => 2 to power 54
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object 
+
+console.log(typeof number);
