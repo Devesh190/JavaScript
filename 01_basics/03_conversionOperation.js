b85158a987ecef94e7a467cc7c8d170c86557a06
Devesh190 <deveshchoudhary373@gmail.com>
2024-05-09 07:32:44 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -33,4 +33,34 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = true
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
+// console.log(stringNumber);
+
+// ************* Operations ****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%2);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// equality check == and comparisons 
+// > < >= <= work differently
+// comparison convert null to a number, treating it as 0.
+// that's why (3) null >= 0 is true and (1) null > 0 is false.
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// === 
+
+console.log("2" === 2);
