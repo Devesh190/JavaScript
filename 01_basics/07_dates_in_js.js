40ee2b0ebd8b36255a8489e75e699dab95cabe8c
Devesh190 <deveshchoudhary373@gmail.com>
2024-05-23 09:06:10 +0000

    Dates

---
+++ 01_basics/07_dates_in_js.js
@@ -0,0 +1,28 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+// console.log(myDate.getHours());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023,0,23)
+// let myCreatedDate = new Date(2023,0,23,5,30)
+let myCreatedDate = new Date("2025-02-14")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));//Seconds
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default',{
+    weekday:"long"
+})
